b1139b67fe564d7037204689d0c13e0c7a50d5a1
bitwaree <bitware@duck.com>
2025-10-11 15:07:07 +0530

    added multiarch retop

--- src/library.c
+++ src/library.c
@@ -5,24 +5,26 @@
 #include <dlfcn.h>
 #include <jni.h>
 
-
 void patch_libmae() {
-    void* dlhandle = 0;
-    do {
-        void* dlhandle = dlopen("libmaesdk.so", RTLD_NOLOAD);
-    } while (dlhandle);
-
+    void* dlhandle = dlopen("libmaesdk.so", RTLD_NOLOAD);
+    if(!dlhandle) return;
     void *libmae_fun = dlsym(dlhandle, "_ZN9Microsoft12Applications6Events19TelemetrySystemBase5startEv");
     if(libmae_fun) {
+#if defined(__x86_64__) || defined(__amd64__)
+        unsigned char retop = 0xC3;
+#elif defined(__i386__) || defined(__i686__) || defined (__x86__)
+        unsigned char retop = 0xC3;
+#elif defined(__aarch64__)
         uint32_t retop = 0xD65F03C0;
-        write_mem(libmae_fun, &retop, 4);
+#elif defined(__arm__)
+        uint32_t retop = 0xE1A0F00E;
+#endif
+        write_mem(libmae_fun, &retop, sizeof(retop));
     }
 }
 
 jint JNI_OnLoad(JavaVM *vm, void *reserved) {
-    {
-        patch_libmae();
-    }
+    patch_libmae();
     return JNI_VERSION_1_6;
 }
 
